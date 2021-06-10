764823c8637713715c8d070b791923df192bb3af
Your Name <lbookatz@hotmail.com>
2021-06-10 11:35:17 +0100

    app.js with random cats

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import faker from "faker";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [cats, setCats] = useState ([]); 
+  const [basket, setBasket] = useState ([]); 
+
+  useEffect(() => {
+    getCats();
+  }, []);
+
+  const getCats = async () => {
+    
+      let response = await fetch("https://api.thecatapi.com/v1/images/search?limit=4")
+      let data = await response.json(); 
+      for (let i = 0; i < data.length; i++) {
+        const cat = data[i];
+        cat["name"] = faker.name.firstName()        
+        cat["age"] = faker.datatype.number(12)
+      }
+      setCats(data)
+  }
+
+  const addToBasket = (item) => {
+    console.log(basket)
+    setBasket([...basket,item])
+    console.log(basket)
+  }
+
+return(
+  <div>
+   
+    <>
+      <h1>Cats 4 lyf</h1>
+    </>
+
+    {cats.map((item, index) => {
+            return  (
+                <>
+                    <h1 key={index}>name:{item.name} Age:{item.age}</h1>
+                    <img src={item.url} alt="cat" width="100px" height="100px"></img>
+                    <button onClick={() => addToBasket(item)}>add to basket</button>
+                    <button onClick={() => logger()}>add to basket</button>
+                </>
+            )
+            })
+       }
+  </div>
+)
 }
 
 export default App;
